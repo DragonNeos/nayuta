c62d733ec0266bcc18a13d8954f30e6d2e56d2b3
xiehanting@gmail.com <xiehanting@gmail.com@ee7c0592-e152-e0e5-1d7e-e0b8cb9b0db9>
2013-01-22 16:14:00 +0000

    HD Mode and GUI modified

---
+++ nayuta --username xiehanting@gmail.com/start-windows-x86.bat
@@ -0,0 +1,42 @@
+@echo off
+rem CD to the path of the command line, this is required when running as an administrator
+cd /D "%~dp0"
+
+set PATH=%PATH%;lib\;lib\windows-x86\
+
+if NOT EXIST "%SystemRoot%\SysWOW64" goto JAVA32
+set key=HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\JavaSoft\Java Runtime Environment
+goto JAVA
+
+:JAVA32
+set key=HKEY_LOCAL_MACHINE\SOFTWARE\JavaSoft\Java Runtime Environment
+
+:JAVA
+set JAVA_CMD=java.exe
+
+rem Checking if the "reg" command is available
+reg /? >NUL
+if ERRORLEVEL 1 goto RUN
+
+set JAVA_VERSION=
+set JAVA_HOME=
+for /f "tokens=3* skip=2" %%a in ('reg query "%key%" /v CurrentVersion') do set JAVA_VERSION=%%a
+for /f "tokens=2* skip=2" %%a in ('reg query "%key%\%JAVA_VERSION%" /v JavaHome') do set JAVA_HOME=%%b
+
+set JAVA_CMD=%JAVA_HOME%\bin\java.exe
+if not exist "%JAVA_CMD%" goto JAVAMISSING
+
+:RUN
+echo Running Jpcsp 32bit...
+"%JAVA_CMD%" -Xmx1024m -Xss2m -XX:MaxPermSize=128m -XX:ReservedCodeCacheSize=64m -Djava.library.path=lib/windows-x86 -jar bin/jpcsp.jar %*
+if ERRORLEVEL 1 goto PAUSE
+goto END
+
+:JAVAMISSING
+echo The required version of Java has not been installed or isn't recognized.
+echo Go to http://java.sun.com to install the 32bit Java JRE.
+
+:PAUSE
+pause
+
+:END
